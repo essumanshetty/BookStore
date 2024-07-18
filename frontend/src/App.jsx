2b1f38f32df9806cce9d5de1d1d594cb2400c899
Suman Shetty <sumanshettysumanshetty65@gmail.com>
2024-07-18 10:29:14 +0530

    Update: Add Edit Book Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,24 +4,29 @@ import Container from "./Container/Container";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import ViewAllBooks from "./Container/ViewAllBooks";
 import AddBook from "./Container/AddBook";
+import EditBook from './Container/EditBook';
 
 function App() {
   const navigate = useNavigate();
 
-  const handleViewAllBooks =()=>{
-    navigate('/view_all_books');
-  }
+  const handleViewAllBooks = () => {
+    navigate("/view_all_books");
+  };
 
   return (
     <div className="container full-page">
       <div className="header-section">
-        <Header/>
+        <Header />
       </div>
       <div className="content">
         <Routes>
-          <Route path="/" element={<Container handleViewAllBooks={handleViewAllBooks}/>} />
+          <Route
+            path="/"
+            element={<Container handleViewAllBooks={handleViewAllBooks} />}
+          />
           <Route path="/view_all_books" element={<ViewAllBooks />} />
           <Route path="/add_book" element={<AddBook />} />
+          <Route path="/edit_book" element={<EditBook/>}/>
         </Routes>
       </div>
     </div>
